58d10736f97cc02626bbc9866d528db4c076b381
Sudhir Govindaraji <sudhirgs121@gmail.com>
2022-11-04 23:12:29 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+
+var config = {    
+    texts: [
+        "Just wanted to be", 
+        "the first one to", 
+        "wish you",
+        "happy birthday",
+        "so",
+        "I can feel superior",
+        "to your other well-wishers.",
+        "So, happy birthday!",
+        "Bhumika (A) Boo ~~",
+    ],
+
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+
+};
